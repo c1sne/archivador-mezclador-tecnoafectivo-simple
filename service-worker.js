78054820942a52b2829f7ac59d567a18da206ff2
Andres Cisneros <c1sne@users.noreply.github.com>
2026-06-28 14:43:48 -0500

    subir lienzo de materiales

--- service-worker.js
+++ service-worker.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "mezclador-tecnoafectivo-v10";
+const CACHE_NAME = "mezclador-tecnoafectivo-v11";
 const APP_FILES = [
   "./",
   "./index.html",
-  "./styles.css?v=10",
-  "./app.js?v=10",
+  "./styles.css?v=11",
+  "./app.js?v=11",
   "./manifest.webmanifest",
   "./icon.svg",
 ];
